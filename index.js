0b2ed751881fe3059321c0d10a2b4dcf0f95e9fb
adriencpr <adriencompare@gmail.com>
2024-12-06 09:58:59 +0100

    test déploiment

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 // routing path
 app.get('/', (req, res) => {
-    res.send('Déploiement continue OKAYYY');
+    res.send('Déploiement continue ');
 });
 
 // Start the server
